adea6a0ea3d3f5bcf7b1c6a7d0e310ee275a6cb8
Максимов Максим <mpmaximov@gmail.com>
2017-12-04 09:14:43 +0200

    os env

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/test')
 def hello():
+	print(os.environ['APP_SETTINGS'])
     return "OK"
 
 if __name__ == '__main__':
