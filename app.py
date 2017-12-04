1cef8802fbd24e1a4e87fe2a67c150a1e5c32f35
Максимов Максим <mpmaximov@gmail.com>
2017-12-04 10:44:59 +0200

    tables

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Intervals
 
 @app.route('/test')
 def hello():
-	print(os.environ['APP_SETTINGS'])
-    return "OK"
+    print("ok")
+    return "OK " + os.environ['APP_SETTINGS']
 
 if __name__ == '__main__':
     app.run()
